ab2bf8e991c2ef47a5f616b2852558b4d6f67e9a
Adnan <adnanali23189@gmail.com>
2024-07-21 16:50:32 +0530

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Coupons - Hi tech Copier Solutions | Digital Xerox Machine Discount Sales",
+  description: "Canon Xerox Kyocera Discounts in Hyderabad amazing best prices coupon codes discount codes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
