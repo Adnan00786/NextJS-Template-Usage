cec07c7998e495f8c9eaebedb340c925b8d027d6
Adnan <adnanali23189@gmail.com>
2024-07-22 06:40:13 +0530

    Major Page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-         Discount Code
+        Your Discount Code is  `Save20`
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -19,8 +19,9 @@ export default function Home() {
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-      <h1 className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-      Your Discount Code is  `Save20`
+      {/* <h1 className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> */}
+      <h1 className="text-3xl">
+      Best Copiers Best Deals
         </h1>
       </div>
       <br />
@@ -43,6 +44,22 @@ export default function Home() {
           </p>
         </a>
 
+        <a
+          href="https://www.google.com/maps/place/Hi-Tech+Copier+Solution/@17.3830356,78.4910666,17z/data=!4m7!3m6!1s0x3bcb99e2271bb90b:0x1a83ecd23468a794!8m2!3d17.3830356!4d78.4955727!15sCmpTaG9wIE5vOiAzLCAxLTc2NiwgbmVhciBCaGFyYXQgcGV0cm9sLCBUaXJtYWxhIEFyY2FkZSwgTmltYm9saWFkZGEsIEthY2hpZ3VkYSwgSHlkZXJhYmFkLCBUZWxhbmdhbmEgNTAwMDI3IgOIAQFaZCJic2hvcCBubyAzIDEgNzY2IG5lYXIgYmhhcmF0IHBldHJvbCB0aXJtYWxhIGFyY2FkZSBuaW1ib2xpYWRkYSBrYWNoaWd1ZGEgaHlkZXJhYmFkIHRlbGFuZ2FuYSA1MDAwMjeSARVjb3BpZXJfcmVwYWlyX3NlcnZpY2WaASNDaFpEU1VoTk1HOW5TMFZKUTBGblNVUkROWEJVUlVwbkVBReABAA!16s%2Fg%2F11j8x1kd0w?coh=210790&entry=tts&g_ep=EgoyMDI0MDUyOS4wKgBIAVAD"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Shop{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            View Loaction of Google Maps
+          </p>
+        </a>
         <a
           href="https://instagram.com/hitechcopier"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
